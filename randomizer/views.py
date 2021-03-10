4971b17b1e8a9a3a3c3513e726b81a0d277cf810
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-03-10 11:46:16 +0100

    added nickname feature

--- randomizer/views.py
+++ randomizer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # import random
 import random
-import csv
+
 
 
 # Create your views here.
@@ -10,11 +10,15 @@ def home(request):
     return render(request, 'randomizer/home.html')
 
 def nickname(request):
-    # get a list of names from name csv file
     
-    with open('media/randomizer/docs/name_file.csv', newline='') as File:  
-         nameList = list(csv.reader(File))
+    f=open("media/randomizer/docs/db.txt", "r")
+    if f.mode == 'r':
+        nicknames = f.read()
+        print(nicknames)
 
 
+
+
+
     
-    return render(request, 'randomizer/nickname.html', {'names':nameList})
+    return render(request, 'randomizer/nickname.html', {'names':nicknames})
