2cb8fc2da06512b7232c5e3acee736eb73f3669b
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-03-11 08:17:58 +0100

    second phase

--- randomizer/views.py
+++ randomizer/views.py
@@ -10,31 +10,47 @@ def home(request):
     return render(request, 'randomizer/home.html')
 
 def nickname(request):
-    
     # added read feature to parse db.txt
     #nicknames = f.read()
-
+    # generate firstname
+    """ position=0
+    flag=False"""
+    length = int(request.GET.get('length',6))
     if request.GET.get('firstname'):
         f=open("media/randomizer/docs/db.txt", "r")
         if f.mode == 'r':
             list_of_names = f.readlines()
-            # print first item in array
-            
 
-    
+
+            # print first item in array
             i = random.randint(0, int(240698))
+            #result = list_of_names[i]
+
+        
+
             
             result = list_of_names[i]
 
-            
+            letters =''
+            letters += result
+            letters = letters.strip()
+            count = len(list(letters))
+            if  count == length:
+                return render(request, 'randomizer/nickname.html', {'result':result})
+     
+
+    print(count)
+    print (letters)
+    
+    return render(request, 'randomizer/nickname.html', {'names':letters, 'count':count})
 
 
-    return render(request, 'randomizer/nickname.html', {'names':result})
+    
+    
+    
+    
+
+    
         
 
-    
 
-
-
-    
-   
