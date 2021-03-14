4192e592b5ea8cc5f9e914baf2982bf855d1f931
Kolawole Ikeoluwa <kolawoleikeoluwa2018@gmail.com>
2021-03-14 21:46:02 +0100

    edited the views.py

--- randomizer/views.py
+++ randomizer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # import random
 import random
+from random import randint
 
 
 
@@ -10,47 +11,36 @@ def home(request):
     return render(request, 'randomizer/home.html')
 
 def nickname(request):
-    # added read feature to parse db.txt
-    #nicknames = f.read()
-    # generate firstname
-    """ position=0
-    flag=False"""
-    length = int(request.GET.get('length',6))
     if request.GET.get('firstname'):
+        
         f=open("media/randomizer/docs/db.txt", "r")
         if f.mode == 'r':
             list_of_names = f.readlines()
-
 
             # print first item in array
             i = random.randint(0, int(240698))
             #result = list_of_names[i]
 
-        
-
-            
             result = list_of_names[i]
+            result = result.strip()
 
-            letters =''
-            letters += result
-            letters = letters.strip()
-            count = len(list(letters))
-            if  count == length:
-                return render(request, 'randomizer/nickname.html', {'result':result})
-     
+    if request.GET.get('length'):
 
-    print(count)
-    print (letters)
-    
-    return render(request, 'randomizer/nickname.html', {'names':letters, 'count':count})
+        f=open("media/randomizer/docs/db.txt", "r")
+        if f.mode == 'r':
+            list_of_names = f.readlines()
+            bag = []
 
 
-    
-    
-    
-    
+            length = int(request.GET.get('length'))
+            for name in list_of_names:
+                word = name.strip()
+                if len(word) == length:
+                    bag.append(word)
+                    
+                    #print(first_word)
+            random_length = len(bag)
+            i = random.randint(0, random_length)
+            result = bag[i]
 
-    
-        
-
-
+    return render(request, 'randomizer/nickname.html', {'names':result})
